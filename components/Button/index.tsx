83cecfd2b8eb60b2e65ff8e5f1f94231cf245635
Valtteri Karesto <valtteri.karesto@gmail.com>
2021-11-22 11:52:03 +0200

    Added some components and new list of tokens with proper names

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,73 +1,133 @@
-import { rgba } from "polished";
 import { styled } from "../../stitches.config";
 
-const Button = styled("button", {
-  all: "unset",
-  display: "inline-flex",
-  fontFamily: "$body",
-  fontWeight: "$body",
-  color: "$black",
-  justifyContent: "center",
-  alignItems: "center",
-  lineHeight: "$one",
-  cursor: "pointer",
-  variants: {
-    variant: {
-      default: {
-        // inverse button is default
-        backgroundColor: "$text",
-        color: "$background",
-      },
-      primary: {
-        backgroundColor: "$primary",
-      },
+const StyledButton = styled(
+  "button",
+  {
+    all: "unset",
+    WebkitTapHighlightColor: "rgba(0,0,0,0)",
+    "::before": {
+      boxSizing: "border-box",
     },
-    size: {
-      sm: {
-        height: "$8",
-        px: "$6",
-      },
-      md: {
-        height: "$10",
-        px: "$8",
-      },
-      lg: {
-        height: "$12",
-        px: "$10",
-      },
+    "::after": {
+      boxSizing: "border-box",
     },
-    outline: {
-      true: {
-        $$outlineColor: "$colors$text",
-        background: "transparent",
-        color: "$$outlineColor",
-        boxShadow: "0px 0px 0px 1px $$outlineColor",
-        "&:hover": {
-          backgroundColor: "$$outlineColor",
+    display: "inline-flex",
+    fontFamily: "$body",
+    fontWeight: "$body",
+    color: "$black",
+    justifyContent: "center",
+    alignItems: "center",
+    lineHeight: 1.1,
+    cursor: "pointer",
+    boxShadow: "0px 0px 0px 1px $$borderColor",
+    variants: {
+      variant: {
+        default: {
+          color: "$background",
+          backgroundColor: "$textA",
+          boxShadow: "0px 0px 0px 1px $colors$textA",
+        },
+        primary: {
+          color: "$black",
+          backgroundColor: "$primary",
+          boxShadow: "0px 0px 0px 1px $colors$primary",
+        },
+      },
+      size: {
+        sm: {
+          height: "$7",
+          px: "$3",
+        },
+        md: {
+          height: "$10",
+          px: "$8",
+        },
+        lg: {
+          height: "$12",
+          px: "$10",
+        },
+      },
+      outline: {
+        true: {
+          backgroundColor: "transparent",
+        },
+      },
+      active: {
+        true: {},
+      },
+      uppercase: {
+        true: {
+          textTransform: "uppercase",
+        },
+      },
+      ghost: {
+        true: {
+          boxShadow: "none",
+          background: "transparent",
         },
       },
     },
-  },
-  compoundVariants: [
-    {
-      outline: true,
-      variant: "primary",
-      css: {
-        $$outlineColor: "$colors$primary",
+    compoundVariants: [
+      {
+        outline: true,
+        variant: "primary",
+        css: {
+          color: "$primary",
+          boxShadow: "0px 0px 0px 1px $colors$primary",
+          backgroundColor: "transparent",
+        },
       },
-    },
-    {
-      outline: true,
+      {
+        outline: true,
+        variant: "default",
+        css: {
+          backgroundColor: "transparent",
+          color: "$text",
+        },
+      },
+      {
+        variant: "default",
+        outline: true,
+        active: true,
+        css: {
+          backgroundColor: "$textA",
+          color: "$background",
+        },
+      },
+      {
+        variant: "primary",
+        outline: true,
+        active: true,
+        css: {
+          backgroundColor: "$primary",
+          color: "$black",
+        },
+      },
+      {
+        variant: "primary",
+        ghost: true,
+        css: {
+          color: "$primary",
+          "&:hover": {
+            backgroundColor: "rgba(255,255,255,.1)",
+            boxShadow: "none",
+          },
+          "&:active": {
+            backgroundColor: "$mauve8",
+            boxShadow: "none",
+          },
+          "&:focus": {
+            boxShadow: "none",
+          },
+        },
+      },
+    ],
+    defaultVariants: {
+      size: "md",
       variant: "default",
-      css: {
-        $$outlineColor: "$text",
-      },
     },
-  ],
-  defaultVariants: {
-    variant: "default",
-    size: "md",
   },
-});
+  { as: "button" }
+);
 
-export default Button;
+export default StyledButton;
