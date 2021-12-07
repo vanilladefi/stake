435686d82e714d100c6ded092c20ec909d5331dd
Joni Juup <jonijuup@gmail.com>
2021-12-07 16:21:04 +0200

    litepaper implementation, elements in place

---
+++ components/Paragraph/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from "../../stitches.config";
+
+const Paragraph = styled("p", {
+  fontFamily: "$body",
+  lineHeight: "$body",
+  color: "$text",
+});
+
+export default Paragraph;
