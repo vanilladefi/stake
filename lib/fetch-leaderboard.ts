f1562f16cab7ce49cc1fc8aab390b914f0b04300
muzam1l <muzamilsofi@outlook.com>
2022-05-19 15:15:12 +0530

    [TEMP] Inline leaderboard calculations for faster testing

--- lib/fetch-leaderboard.ts
+++ lib/fetch-leaderboard.ts
@@ -1,6 +1,8 @@
-import { isAddress } from "@vanilladefi/core-sdk";
-import { getLeaderboard } from "@vanilladefi/stake-sdk";
-import { BigNumber } from "ethers";
+import { epoch, isAddress } from "@vanilladefi/core-sdk";
+import { getJuiceStakingContract, LeaderBoard, networks, Options } from '@vanilladefi/stake-sdk';
+import { TypedEvent } from '@vanilladefi/stake-sdk/lib/types/juicenet/common';
+import { BigNumber, ethers, providers } from "ethers";
+import { isHexString } from 'ethers/lib/utils';
 import type {
   ILeaderboard,
   JuicerColumn,
@@ -9,6 +11,7 @@ import type {
 import { state } from "../state";
 import { getBlockByTimestamp } from "../utils/helpers";
 import { juiceEpoch } from "./config";
+
 
 export const getLeaderboardData = async (
   range: LeaderboardRange
@@ -79,3 +82,209 @@ export const fetchLeaderboard = async (): Promise<ILeaderboard> => {
     dailyData: await getLeaderboardData("daily"),
   };
 };
+
+
+
+/**
+ * Fetches an individual user's JUICE delta in given block interval.
+ *
+ * @param userAddress
+ * @param from
+ * @param to
+ * @param options
+ * @returns User's JUICE delta as a BigNumber.
+ */
+export const getUserJuiceDelta = async (
+  userAddress: string,
+  from?: string | number,
+  to?: string | number,
+  trimPreviousStake = true,
+  options?: Options,
+): Promise<BigNumber> => {
+  const parsedFrom = await parseBlockTagToBlockNumber(from || epoch, options)
+  const parsedTo = await parseBlockTagToBlockNumber(to || 'latest', options)
+  const blockThreshold = options?.blockThreshold || 1000
+
+  const contract = getJuiceStakingContract(options)
+  const deltaByToken: Record<string, BigNumber> = {}
+  const latestUnstakeByToken: Record<string, number> = {}
+  const firstUnstakeByToken: Record<string, number> = {}
+  let delta = BigNumber.from(0)
+
+  const stakeFilter: ethers.EventFilter =
+    contract.filters.StakeAdded(userAddress)
+  const unStakeFilter: ethers.EventFilter =
+    contract.filters.StakeRemoved(userAddress)
+
+  // Split requests to ranges because of RPCs having block depth limits
+  const ranges: Array<{ startBlock: number; endBlock: number }> = []
+  let startBlock = parsedFrom
+  let endBlock =
+    parsedTo > startBlock + blockThreshold
+      ? startBlock + blockThreshold
+      : parsedTo
+  let oldEndBlock = endBlock
+  while (endBlock < parsedTo - blockThreshold) {
+    ranges.push({ startBlock, endBlock })
+    oldEndBlock = endBlock
+    endBlock =
+      endBlock + blockThreshold > parsedTo
+        ? parsedTo
+        : endBlock + blockThreshold
+    startBlock = oldEndBlock + 1
+  }
+  ranges.push({ startBlock, endBlock })
+
+  const unStakes: ethers.Event[] | TypedEvent<any[]>[] = (
+    await Promise.all(
+      ranges.map((range) =>
+        contract.queryFilter(unStakeFilter, range.startBlock, range.endBlock),
+      ),
+    )
+  ).flat()
+
+  // First, filter every realized JUICE profit
+  unStakes.forEach((event) => {
+    if (event?.args && event?.blockNumber) {
+      const { args, blockNumber } = event
+      const { unstakedDiff, token } = args
+
+      if (deltaByToken[token]) {
+        deltaByToken[token] = deltaByToken[token].add(unstakedDiff)
+      } else {
+        deltaByToken[token] = unstakedDiff
+      }
+
+      if (
+        !firstUnstakeByToken[token] ||
+        firstUnstakeByToken[token] > blockNumber
+      ) {
+        firstUnstakeByToken[token] = blockNumber
+      }
+
+      if (
+        !latestUnstakeByToken[token] ||
+        latestUnstakeByToken[token] < blockNumber
+      ) {
+        latestUnstakeByToken[token] = blockNumber
+      }
+    }
+  })
+
+  if (Object.keys(deltaByToken).length > 0) {
+    const stakes: ethers.Event[] | TypedEvent<any[]>[] = (
+      await Promise.all(
+        ranges.map((range) =>
+          contract.queryFilter(stakeFilter, range.startBlock, range.endBlock),
+        ),
+      )
+    ).flat()
+
+    // Remove stakes from delta
+    stakes.forEach((event) => {
+      if (event?.args && event?.blockNumber) {
+        const { args, blockNumber } = event
+        const { unstakedDiff, token } = args
+        if (
+          Object.keys(deltaByToken).includes(token) &&
+          latestUnstakeByToken[token] > blockNumber
+        ) {
+          // Remove the stakes previous to first unstake from the delta if trimPreviousStake === true
+          if (
+            !(trimPreviousStake && firstUnstakeByToken[token] < blockNumber)
+          ) {
+            deltaByToken[token] = deltaByToken[token].add(unstakedDiff)
+          }
+        }
+      }
+    })
+
+    delta = Object.values(deltaByToken).reduce((_previous, _current) =>
+      _previous.add(_current),
+    )
+  }
+
+  return delta
+}
+
+
+export const getLeaderboard = async (
+  from?: string | number,
+  to?: string | number,
+  limit = 10,
+  trimPreviousStake = true,
+  options?: Options,
+): Promise<LeaderBoard> => {
+  const users = await getUsers(from, to, options)
+  let juiceDeltas: LeaderBoard = await Promise.all(
+    Array.from(users).map(async (user) => ({
+      user: user,
+      delta: await getUserJuiceDelta(
+        user,
+        from,
+        to,
+        trimPreviousStake,
+        options,
+      ),
+    })),
+  )
+  juiceDeltas = juiceDeltas.sort((a, b) => {
+    if (a.delta.gt(b.delta)) {
+      return -1
+    } else if (a.delta.eq(b.delta)) {
+      return 0
+    } else {
+      return 1
+    }
+  })
+  return juiceDeltas.slice(0, limit)
+}
+
+export const getUsers = async (
+  from: string | number = epoch,
+  to?: string | number,
+  options?: Options,
+): Promise<Set<string>> => {
+  const contract = getJuiceStakingContract(options)
+  const users: Set<string> = new Set()
+
+  const stakeFilter: ethers.EventFilter = contract.filters.StakeAdded()
+  const stakes: ethers.Event[] | TypedEvent<any[]>[] =
+    await contract.queryFilter(stakeFilter, from, to)
+
+  stakes.forEach((event) => {
+    users.add(event?.args?.user)
+  })
+
+  return users
+}
+
+
+
+export const parseBlockTagToBlockNumber = async (
+  blockTag: string | number,
+  options?: Options,
+): Promise<number> => {
+  let blockNumber = 0
+  let latestBlockNumber = 0
+  const polygonProvider =
+    options?.polygonProvider || providers.getDefaultProvider(networks.mainnet)
+
+  if (blockTag === 'latest' || blockTag < 0) {
+    latestBlockNumber = await polygonProvider.getBlockNumber()
+  }
+
+  if (isHexString(blockTag)) {
+    blockNumber = parseInt(String(blockTag), 16)
+  } else if (blockTag === 'latest') {
+    blockNumber = latestBlockNumber
+  } else if (typeof blockTag === 'number') {
+    if (blockTag < 0) {
+      blockNumber = blockTag + latestBlockNumber
+    } else {
+      blockNumber = blockTag
+    }
+  }
+
+  return blockNumber
+}
