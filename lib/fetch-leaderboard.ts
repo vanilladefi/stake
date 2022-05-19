0b7ae9fabb5fbac49e6036aa5b4672d100f5cb7d
muzam1l <muzamilsofi@outlook.com>
2022-05-19 16:26:42 +0530

    Juice pressed is overall profit only

--- lib/fetch-leaderboard.ts
+++ lib/fetch-leaderboard.ts
@@ -9,7 +9,7 @@ import type {
   LeaderboardRange,
 } from "../components/Leaderboard";
 import { state } from "../state";
-import { getBlockByTimestamp } from "../utils/helpers";
+import { formatJuice, getBlockByTimestamp } from "../utils/helpers";
 import { juiceEpoch } from "./config";
 
 
@@ -37,6 +37,7 @@ export const getLeaderboardData = async (
     const from =
       range === "weekly" ? before7D : range === "daily" ? before1D : juiceEpoch;
 
+    console.log(range)
     const leaderboard = await getLeaderboard(from, "latest", 10, false, {
       signerOrProvider: provider,
       optionalAddress: optionalAddress || undefined,
@@ -106,10 +107,6 @@ export const getUserJuiceDelta = async (
   const blockThreshold = options?.blockThreshold || 1000
 
   const contract = getJuiceStakingContract(options)
-  const deltaByToken: Record<string, BigNumber> = {}
-  const latestUnstakeByToken: Record<string, number> = {}
-  const firstUnstakeByToken: Record<string, number> = {}
-  let delta = BigNumber.from(0)
 
   const stakeFilter: ethers.EventFilter =
     contract.filters.StakeAdded(userAddress)
@@ -143,66 +140,41 @@ export const getUserJuiceDelta = async (
     )
   ).flat()
 
-  // First, filter every realized JUICE profit
-  unStakes.forEach((event) => {
-    if (event?.args && event?.blockNumber) {
-      const { args, blockNumber } = event
-      const { unstakedDiff, token } = args
-
-      if (deltaByToken[token]) {
-        deltaByToken[token] = deltaByToken[token].add(unstakedDiff)
-      } else {
-        deltaByToken[token] = unstakedDiff
-      }
-
-      if (
-        !firstUnstakeByToken[token] ||
-        firstUnstakeByToken[token] > blockNumber
-      ) {
-        firstUnstakeByToken[token] = blockNumber
-      }
-
-      if (
-        !latestUnstakeByToken[token] ||
-        latestUnstakeByToken[token] < blockNumber
-      ) {
-        latestUnstakeByToken[token] = blockNumber
-      }
-    }
-  })
-
-  if (Object.keys(deltaByToken).length > 0) {
-    const stakes: ethers.Event[] | TypedEvent<any[]>[] = (
-      await Promise.all(
-        ranges.map((range) =>
-          contract.queryFilter(stakeFilter, range.startBlock, range.endBlock),
-        ),
-      )
-    ).flat()
-
-    // Remove stakes from delta
-    stakes.forEach((event) => {
-      if (event?.args && event?.blockNumber) {
-        const { args, blockNumber } = event
-        const { unstakedDiff, token } = args
-        if (
-          Object.keys(deltaByToken).includes(token) &&
-          latestUnstakeByToken[token] > blockNumber
-        ) {
-          // Remove the stakes previous to first unstake from the delta if trimPreviousStake === true
-          if (
-            !(trimPreviousStake && firstUnstakeByToken[token] < blockNumber)
-          ) {
-            deltaByToken[token] = deltaByToken[token].add(unstakedDiff)
-          }
-        }
-      }
-    })
-
-    delta = Object.values(deltaByToken).reduce((_previous, _current) =>
-      _previous.add(_current),
+  const stakes: ethers.Event[] | TypedEvent<any[]>[] = (
+    await Promise.all(
+      ranges.map((range) =>
+        contract.queryFilter(stakeFilter, range.startBlock, range.endBlock),
+      ),
     )
-  }
+  ).flat()
+
+  let delta = BigNumber.from(0)
+
+//   S    U S   U  US
+//  -2   .8 -1  1  05
+
+  unStakes.forEach((event) => {
+    const { args } = event
+    if (!args) return
+    
+    const { unstakedDiff, user } = args
+    if (user === '0xE0F5466AF66AbA62f5e2027Cbb294f54e60276bC') {
+      console.log(formatJuice(unstakedDiff))
+    }
+
+    delta = delta.add(unstakedDiff)
+  })
+  stakes.forEach(event => {
+    const { args } = event
+    if (!args) return
+
+    const { unstakedDiff, user } = args
+    if (user === '0xE0F5466AF66AbA62f5e2027Cbb294f54e60276bC') {
+      console.log(formatJuice(unstakedDiff))
+    }
+
+    delta = delta.add(unstakedDiff)
+  })
 
   return delta
 }
